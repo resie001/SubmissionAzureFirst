4fd2a5493e1f70da5cd877537a999b0a87b30c6e
resie001 <aderesie.m.s@gmail.com>
2019-09-23 22:52:11 +0700

    pertama

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register Lab Chevalier</title>
+    <style type="text/css">
+        body {
+            padding-left : 2%;
+        }
+    </style>
+</head>
+<body>
+
+    <h1>Form Register Anggota Lab Chevalier <br>Telkom University</h1>
+    <p>Tolong isi form pendaftaran dengan serius dan teliti!</p>
+    <form action="" method="POST">
+        Nama <input type="text" name="name"><br><br>
+        Email <input type="email" name="email"><br><br>
+        Divisi <input type="text" name="division"><br><br>
+        <input type="submit" name="submit" value="Submit"> <input type="submit" name="loadData" value="Load Data"> <input type="reset">
+    </form>
+
+<?php
+
+    // $host = 
+
+    // try {
+
+    // }
+
+    // if (isset($_POST['submit'])) {
+            
+    //     try {
+
+    //     }
+    // }
+
+?>
+    
+</body>
+</html>
