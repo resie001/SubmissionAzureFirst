1f6b35db9a5bae5c610689626cc4cb613f50d49a
resie001 <aderesie.m.s@gmail.com>
2019-09-24 07:18:57 +0700

    kedua

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+
+    $host = "serverdicoding.database.windows.net";
+    $user = "aderesie";
+    $pass = "Corazon123";
+    $db   = "submission";
+
+    try{
+        $conn = new PDO("sqlsrv:server = $host; Database =$db", $user, $pass);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: ".$e;
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,26 +31,63 @@
     <h1>Form Register Anggota Lab Chevalier <br>Telkom University</h1>
     <p>Tolong isi form pendaftaran dengan serius dan teliti!</p>
     <form action="" method="POST">
-        Nama <input type="text" name="name"><br><br>
-        Email <input type="email" name="email"><br><br>
-        Divisi <input type="text" name="division"><br><br>
+        Nama <input type="text" name="name" required><br><br>
+        Email <input type="email" name="email" required><br><br>
+        Divisi <input type="text" name="division" required><br><br>
         <input type="submit" name="submit" value="Submit"> <input type="submit" name="loadData" value="Load Data"> <input type="reset">
     </form>
 
 <?php
 
-    // $host = 
+    if (isset($_POST['submit'])) {
+        try{
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $division = $_POST['division'];
+            $date = date("Y-m-d");
 
-    // try {
+            $sql_insert = "INSERT INTO user ( name, division, email, date) value (?,?,?,?)";
+            $stmt = $conn->prepare($sql_insert);
+            $stmt->bindValue(1,$name);
+            $stmt->bindValue(2, $division);
+            $stmt->bindValue(3, $email);
+            $stmt->bindValue(4, $date);
+            $stmt->execute();
 
-    // }
-
-    // if (isset($_POST['submit'])) {
-            
-    //     try {
-
-    //     }
-    // }
+            echo "<h1>Selamat Kamu Telah Mendaftar di Lab Chevalier</h1>";
+        } catch(Exception $e){
+            echo "Failed: ".$e;
+        }
+    } else if (isset($_POST['loadData'])) {
+        try {
+            $sql_select = "SELECT * FROM user";
+            $stmt = $conn->query($sql_select);
+            $registrans = $stmt->fetchAll();
+            if (count($registrans) > 0) {
+                echo "<h1>Orang Yang Telah Mendaftar di Chevalier Lab</h1>";
+                echo "<table>";
+                echo "<tr>";
+                echo "<th>Nama</th>";
+                echo "<th>Divisi</th>";
+                echo "<th>Email</th>";
+                echo "<th>Date</th>";
+                echo "</tr>";
+                foreach($registrans as $value){
+                    echo "<tr>";
+                    echo "<td>".$value['name']."</td>";
+                    echo "<td>".$value['division']."</td>";
+                    echo "<td>".$value['email']."</td>";
+                    echo "<td>".$value['date']."</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "<h1>Belum Ada Yang Mendaftar di Lab Chevalier</h1>";
+            }
+        } catch(Exception $e){
+            echo "Failed: ".$e;
+        }
+    }
 
 ?>
     
