65af5d6bfe69856150e3d04467fe09e54864d3d2
resie001 <aderesie.m.s@gmail.com>
2019-09-24 07:35:59 +0700

    keempat

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     $pass = "Corazon123";
     $db   = "submission";
 
-    try{
-        $conn = new PDO("sqlsrv:server = $host; Database =$db", $user, $pass);
+    try {
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch(Exception $e) {
-        echo "Failed: ".$e;
+        echo "Failed: " . $e;
     }
 
 ?>
