201ce5db5de98dbc042e2cd83458c13646d8836b
resie001 <aderesie.m.s@gmail.com>
2019-09-24 13:01:56 +0700

    revisi15

--- database.sql
+++ database.sql
@@ -1,7 +1,8 @@
 create table [dbo].[submission](
-    id INT NOT NULL PRIMARY KEY(id),
+    id INT NOT NULL IDENTITY(1,1),
     name VARCHAR(50),
     email VARCHAR(50),
     division VARCHAR(50),
-    date DATE NOT NULL
+    date DATE NOT NULL,
+    PRIMARY KEY(id)
 );
